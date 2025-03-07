ee79d2564b197545a45347b318c4b56932c0015f
Guillermo Mercado <guillermo.mercado26@gmail.com>
2025-03-06 23:28:35 -0300

    style: add modal functionality with fade animations and no-scroll behavior for image gallery

--- build/js/app.js
+++ build/js/app.js
@@ -17,25 +17,36 @@ function crearGaleria(){
         }
 
         galeria.appendChild(imagen);
-        console.log(imagen);
     }
 }
 
 function mostrarImagen(i){
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'Imagen galeria';
+    
     // Generar Modal
 
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
-
     modal.onclick = cerrarModal;
+    modal.appendChild(imagen);
+
     // Agregar al HTML
     const body = document.querySelector('body');
+    body.classList.add('no-scroll');
     body.appendChild(modal);
+    
 
     console.log(modal);
 }
 
 function cerrarModal(){
     const modal = document.querySelector('.modal');
-    modal?.remove();
+    modal.classList.add('fadeOut');
+    setTimeout(() => {
+        modal?.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('no-scroll');
+    }, 500)
 }
