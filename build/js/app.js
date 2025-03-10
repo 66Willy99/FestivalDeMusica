1753376005c720391f61b1530688c2510994a50e
Guillermo Mercado <guillermo.mercado26@gmail.com>
2025-03-09 23:27:04 -0300

    style: add lazy loading and set dimensions for images in app.js and index.html

--- build/js/app.js
+++ build/js/app.js
@@ -24,9 +24,12 @@ function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
     for(let i = 1; i<=CANTIDAD_IMAGENES; i++){
         const imagen = document.createElement('IMG');
+        imagen.loading = 'lazy';
+        imagen.width = '300';
+        imagen.height = '200';
         imagen.src = `src/img/gallery/full/${i}.jpg`;
         imagen.alt = 'Imagen galeria';
-
+        
         //Evemt Handler
         imagen.onclick = function(){
             console.log(`diste click...${i}`);
